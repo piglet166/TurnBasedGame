4f48d7f866e974c3c1fe9dcdfaba3b01e40bdb39
piglet166 <35857842+piglet166@users.noreply.github.com>
2019-08-27 17:31:11 -0400

    Added Some Controllers

---
+++ TB Shell/Assets/Scripts/Characters/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    
+    CharacterStats stats;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Death(int h) {
+
+        if (h <= 0) {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public int turnWeight(int armour, int weapon) {
+        int weight = armour + weapon;
+
+        return weight;
+    }
+}
---
+++ TB Shell/Assets/Scripts/Characters/CharacterStats.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public CharacterController controller;
+
+    [Header("Base Stats")]
+    public int Resistance;
+    public int Agility;
+    public int Damage;
+    public int Endurance;
+    public int Skill;
+
+    [Header("Character Stats")]
+    [SerializeField]
+    private int health, speed, minDamage, maxDamage, parryChance;
+
+    //[Header("Turn Effects")]
+    //public int damageRecieved;
+    //public int weaponDamageChange;
+    //public int armourChange;
+    //public int weaponSpeedChange;
+
+    private void initHealth() {
+        health = Resistance * 4;
+    }
+    private void initSpeed(int armour, int weapon) {
+        speed = (Agility * 2) - (controller.turnWeight(armour, weapon) / Endurance);
+    }
+    private void initDamage(int weapon) {
+        maxDamage = Damage + weapon;
+        minDamage = maxDamage - (maxDamage / 4);
+    }
+    private void initParryChance() {
+        parryChance = (Skill / 2) * 10;
+    }
+}
