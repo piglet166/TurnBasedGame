1efb69928e6910593447060fbaa1eacb262168ac
piglet166 <35857842+piglet166@users.noreply.github.com>
2019-08-28 12:13:00 -0400

    Added character scripts for different units

---
+++ TB Shell/Assets/Scripts/Characters/Archer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : MonoBehaviour
+{
+    CharacterStats baseStats;
+    private bool knocked;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        knocked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void BaseStats() {
+
+        baseStats.Resistance = 1;
+        baseStats.Agility = 1;
+        baseStats.Damage = 1;
+        baseStats.Endurance = 1;
+        baseStats.Skill = 1;
+    }
+
+    public void Knock() {
+        knocked = true;
+    }
+
+    public void Release() {
+        knocked = false;
+    }
+
+    public void Fire() {
+        knocked = false;
+    }
+
+    
+}
---
+++ TB Shell/Assets/Scripts/Characters/HeavySoldier.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavySoldier : MonoBehaviour
+{
+
+    CharacterStats baseStats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void BaseStats() {
+
+        baseStats.Resistance = 1;
+        baseStats.Agility = 1;
+        baseStats.Damage = 1;
+        baseStats.Endurance = 1;
+        baseStats.Skill = 1;
+    }
+}
