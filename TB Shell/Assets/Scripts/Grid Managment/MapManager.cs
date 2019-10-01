1d4a7696aef4c3a775e8a0ebc1bf73a1fd96d469
Dylan Campfield <d.campfield@icloud.com>
2019-10-01 11:41:55 -0400

    Moving Scenes and Scripts

---
+++ TB Shell/Assets/Scripts/Grid Managment/MapManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public GameObject hexPrefab;
+
+    //Size of map in terms of Hex tile size,
+    //NOT representative of world space
+    //(tiles might be more or less than 1 Unity World unit)
+    int width = 20;
+    int height = 20;
+
+    float xOffset = 1.03f;
+    float zOffset = 0.892f;
+    
+    
+    void Start()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float xPos = x * xOffset;
+
+                //If on an odd row..
+                if (y % 2 == 1)
+                {
+                    xPos += xOffset/2f;
+                }
+                //setting position with no rotation
+                GameObject hex_GO = (GameObject)Instantiate(hexPrefab, new Vector3 (xPos, 0, y * zOffset), Quaternion.identity);
+
+                //Organization
+                hex_GO.name = "Hex_" + x + "_" + y;
+
+                //Organization
+                hex_GO.transform.SetParent(this.transform);
+            }
+        }
+
+
+    }
+
+    
+    void Update()
+    {
+
+
+
+    }
+}
---
+++ TB Shell/Assets/Scripts/Grid Managment/MouseManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            GameObject hitObject = hitInfo.collider.transform.gameObject;
+
+            Debug.Log("Raycast hit: " + hitObject.name);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                MeshRenderer mr = hitObject.GetComponentInChildren<MeshRenderer>();
+
+                if (mr.material.color == Color.red)
+                {
+                    mr.material.color = Color.white;
+                }
+                else
+                {
+                    mr.material.color = Color.red;
+                }
+            }
+        }
+    }
+}
