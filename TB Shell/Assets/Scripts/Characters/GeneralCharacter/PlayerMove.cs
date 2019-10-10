e4321d16f856524e813f2cc125ff15d903d0a9a6
piglet166 <35857842+piglet166@users.noreply.github.com>
2019-10-10 11:12:47 -0400

    Added some code for Turn Manager

--- TB Shell/Assets/Scripts/Characters/GeneralCharacter/PlayerMove.cs
+++ TB Shell/Assets/Scripts/Characters/GeneralCharacter/PlayerMove.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class PlayerMove : Movement
 {
+    public TurnManager mother;
+    int myTurn;
+    bool mayI;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Init();        
+        myTurn = 0;
+        mayI = false;
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!moving) {
-            BreadthFirstSeach();
-            CheckMouse();
-        } else {
-            Move();
+        if (MotherMayI()) {
+            if (!moving) {
+                BreadthFirstSeach();
+                CheckMouse();
+
+            } else {
+                Move();
+            }
+
+            EndTurn();
         }
     }
 
@@ -35,6 +46,37 @@ public class PlayerMove : Movement
                     }
                 }
             }
+            MovementBugCatcher(true);
+        }
+    }
+
+    bool MotherMayI() {
+        if (mother.GetTurn() > myTurn) {
+            mayI = false;
+            Debug.Log("No");
+        } else {
+            mayI = true;
+        }
+
+        return mayI;
+    }
+
+    void EndTurn() {
+        if(moved > 1) {
+            mother.FinishTurn(myTurn);
+            TurnReset();
+            Debug.Log("Turn Ended");
+        }else if(moved > 0 && attacked) {
+            mother.FinishTurn(myTurn);
+            TurnReset();
+            Debug.Log("Turn Ended");
+        }
+    }
+
+    void EndTurn(bool command) {
+        if (command) {
+            mother.FinishTurn(myTurn);
+            TurnReset();
         }
     }
 }
