9404fba56510f707e7e032b6517d700f9f9a8f61
Dylan Campfield <d.campfield@icloud.com>
2019-10-16 02:31:12 -0400

    Finishing CameraController for World Cam

--- TB Shell/TB Shell/Assets/Scripts/CameraController.cs
+++ TB Shell/TB Shell/Assets/Scripts/CameraController.cs
@@ -4,25 +4,109 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform cameraTransform;
+
+    public float normalSpeed;
+    public float fastSpeed;
     public float movementSpeed;
     public float movementTime;
+    public float rotationAmount;
+    public Vector3 zoomAmount;
 
     public Vector3 newPosition;
+    public Vector3 newZoom;
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
+    public Vector3 rotateStartPosition;
+    public Vector3 rotateCurrentPosition;
+    public Quaternion newRotation;
+
     // Start is called before the first frame update
     void Start()
     {
         newPosition = transform.position;
+        newRotation = transform.rotation;
+        newZoom =     cameraTransform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleMovementInput();
+        HandleKeyboardInput();
+        HandleMouseInput();
     }
 
-    void HandleMovementInput()
+    void HandleMouseInput()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        //Handling zoom via scroll wheel
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            newZoom += Input.mouseScrollDelta.y * zoomAmount;
+        }
+
+        //Handling camera position via mouse click and drag
+        if (Input.GetMouseButtonDown(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float point; //point of Raycast from mouse position
+
+            if (plane.Raycast(ray, out point))
+            {
+                dragStartPosition = ray.GetPoint(point); //set this point as our Start
+            }
+
+        }
+        if (Input.GetMouseButton(0)) //mouse still held down
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //cast another ray
+
+            float point; //point of Raycast from mouse position
+
+            if (plane.Raycast(ray, out point))
+            {
+                dragCurrentPosition = ray.GetPoint(point);
+
+                newPosition = transform.position + (dragStartPosition - dragCurrentPosition);
+            }
+        }
+
+        //Handling rotation via middle mouse button click
+        if (Input.GetMouseButtonDown(2))
+        {
+            rotateStartPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(2))
+        {
+            rotateCurrentPosition = Input.mousePosition;
+
+            Vector3 difference = rotateStartPosition - rotateCurrentPosition;
+
+            rotateStartPosition = rotateCurrentPosition;
+
+            //Rotating counter direction to the drag
+            newRotation *= Quaternion.Euler(Vector3.up * (-difference.x / 5f));
+        }
+    }
+
+    void HandleKeyboardInput()
+    {
+        //Increase camera movement speed when holding Left Shift
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = fastSpeed;
+        }
+        else
+        {
+            movementSpeed = normalSpeed;
+        }
+
+        //Handling camera movement via WASD and Arrow Keys
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             newPosition += (transform.forward * movementSpeed);
         }
@@ -39,6 +123,29 @@ public class CameraController : MonoBehaviour
             newPosition += (transform.right * movementSpeed);
         }
 
-        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementSpeed);
+        //Handling rotation via Keyboard
+        if (Input.GetKey(KeyCode.Q))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+        }
+
+        //Handling zoom via Keyboard
+        if (Input.GetKey(KeyCode.R))
+        {
+            newZoom += zoomAmount;
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            newZoom += -zoomAmount;
+        }
+
+        //Lerping movements to new positions
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
     }
 }
