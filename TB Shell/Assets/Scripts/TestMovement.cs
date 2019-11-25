d68ea2da6c7e3fa0a496e96dd5afa4dea4a2fdc1
dylancampfield <47644404+dylancampfield@users.noreply.github.com>
2019-11-25 00:41:25 -0500

    Anim work
    pushing Desktop to Laptop

---
+++ TB Shell/Assets/Scripts/TestMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    public Animator anim;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.M))
+        {
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
+
+        if (Input.GetKey(KeyCode.N))
+        {
+            anim.SetBool("Attacking", true);
+        }
+        else
+        {
+            anim.SetBool("Attacking", false);
+        }
+    }
+}
